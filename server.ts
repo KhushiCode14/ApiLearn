120f3bb096189a2244e7c81466e2242c41f4ea32
khushi Gupta <khushikumarigupta463@gmail.com>
2024-11-22 10:44:48 +0530

    add config

--- server.ts
+++ server.ts
@@ -1,8 +1,9 @@
 import app from "./src/app";
-
+import { config } from "./src/config/config";
+const PORT = config.port || 3000
 const startServer = ()=>{
-    app.listen(3000, () => {
-        console.log("Server started on port 3000");
+    app.listen(PORT , () => {
+        console.log(`Server started on port 3000 or ${PORT}`);
     });
 }
 
